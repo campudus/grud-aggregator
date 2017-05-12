3fa413814f9bfcf52152f434f0ee6602cff0b2b8
Joern Bernhardt <jb@campudus.com>
2017-05-12 18:22:43 +0200

    Add exclude column feature
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

---
+++ src/exclude.js
@@ -0,0 +1,44 @@
+import {filter, flatten, flatMap, mapValues, transform} from "lodash";
+
+export function exclude(options = {}) {
+  const {paths = [], preserveConcats = true} = options;
+
+  return data => {
+    const tablesToKickNames = flatten(paths.filter(p => p.length === 1));
+    const tablesToKickIds = flatMap(tablesToKickNames, name => filter(data, t => t.name === name).map(t => t.id));
+    const result = transform(data, (allTables, table) => {
+      if (!tablesToKickIds.includes(table.id)) {
+        const columnNamesToRemove = paths.filter(p => p[0] === table.name).map(p => p[1]);
+        allTables[table.id] = removeColumns(table, columnNamesToRemove, tablesToKickIds);
+      }
+    }, {});
+    return result;
+  };
+
+  function removeColumns(table, columnNamesToRemove, removeLinksToTableIds) {
+    const columnsToKickIndices = transform(table.columns, (ids, column, idx) => {
+      const shouldKickColumn = columnNamesToRemove.includes(column.name);
+      const shouldKickLink = column.kind === "link" && removeLinksToTableIds.includes(column.toTable);
+      const shouldKickConcat = !preserveConcats && column.kind === "concat";
+      if (shouldKickColumn || shouldKickLink || shouldKickConcat) {
+        ids.push(idx);
+      }
+    }, []);
+    const preservedColumns = filter(table.columns, (column, idx) => !columnsToKickIndices.includes(idx));
+    const transformedRows = mapValues(table.rows, row => {
+      const newValues = transform(row.values, (values, v, idx) => {
+        if (!columnsToKickIndices.includes(idx)) {
+          values.push(v);
+        }
+      }, []);
+      return Object.assign({}, row, {values: newValues});
+    });
+    return Object.assign({},
+      table,
+      {
+        columns: preservedColumns,
+        rows: transformedRows
+      }
+    );
+  }
+}
---
+++ src/exclude.spec.js
@@ -0,0 +1,168 @@
+import tablesFixture from './__tests__/tablesFixture.json';
+import fixtureDeletedTable from './__tests__/excludeFixture-deletedTableAndLinks.json';
+import fixtureNoConcats from './__tests__/excludeFixture-noConcats.json';
+import fixtureDeletedColumn from './__tests__/excludeFixture-deletedColumn.json';
+import fixtureDeletedLinkColumn from './__tests__/excludeFixture-deletedLinkColumn.json';
+import fixtureDeletedMultipleColumns from './__tests__/excludeFixture-deletedMultipleColumns.json';
+import fixtureDeletedMultipleColumnsInTables from './__tests__/excludeFixture-deletedMultipleColumnsInTables.json';
+import fixtureDeletedColumnsAndConcats from './__tests__/excludeFixture-deletedColumnsAndConcats.json';
+import {exclude} from './exclude';
+import expect from 'must';
+
+describe('exclude', () => {
+
+  it('returns a function, so we can put it into Promise.then chains', () => {
+    expect(exclude()).to.be.a.function();
+  });
+
+  it('can pass data through, if no options were specified', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude())
+      .then(data => {
+        expect(data).to.eql(tablesFixture);
+      });
+  });
+
+  it('passes data through, if no exclude.paths was specified', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({}))
+      .then(data => {
+        expect(data).to.eql(tablesFixture);
+      });
+  });
+
+  it('passes data through, if exclude.paths is empty', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths: []
+      }))
+      .then(data => {
+        expect(data).to.eql(tablesFixture);
+      });
+  });
+
+  it('passes data through, if exclude.paths is non existent', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths: [
+          ['someTable', 'non-existent']
+        ]
+      }))
+      .then(data => {
+        expect(data).to.eql(tablesFixture);
+      });
+  });
+
+  it('can exclude a single table', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths: [
+          ['anotherTestTable']
+        ]
+      }))
+      .then(data => {
+        expect(data).to.eql(fixtureDeletedTable);
+      });
+  });
+
+  it('can exclude all columns by selecting a table so the result is an empty object', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths: [
+          ['testTable'],
+          ['anotherTestTable'],
+          ['thirdTestTable']
+        ]
+      }))
+      .then(data => {
+        expect(data).to.eql({});
+      });
+  });
+
+  it('can exclude all concat columns', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({preserveConcats: false}))
+      .then(data => {
+        expect(data).to.eql(fixtureNoConcats);
+      });
+  });
+
+  it('can exclude a simple column', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths:[
+          ['testTable', 'mlShorttext']
+        ]
+      }))
+      .then(data => {
+        expect(data).to.eql(fixtureDeletedColumn);
+      });
+  });
+
+  it('can exclude a link column', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths:[
+          ['testTable', 'someLink']
+        ]
+      }))
+      .then(data => {
+        expect(data).to.eql(fixtureDeletedLinkColumn);
+      });
+  });
+
+  it('can exclude multiple columns in a table', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths:[
+          ['testTable', 'slAttachment'],
+          ['testTable', 'mlShorttext']
+        ]
+      }))
+      .then(data => {
+        expect(data).to.eql(fixtureDeletedMultipleColumns);
+      });
+  });
+
+  it('can exclude multiple columns in multiple tables', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths:[
+          ['testTable', 'slShorttext'],
+          ['thirdTestTable', 'someNumber'],
+          ['testTable', 'someLink']
+        ]
+      }))
+      .then(data => {
+        expect(data).to.eql(fixtureDeletedMultipleColumnsInTables);
+      });
+  });
+
+  it('can exclude columns and remove concats at the same time', () => {
+    return Promise
+      .resolve(tablesFixture)
+      .then(exclude({
+        paths:[
+          ['testTable', 'slShorttext'],
+          ['thirdTestTable', 'someNumber'],
+          ['testTable', 'someLink']
+        ],
+        preserveConcats: false
+      }))
+      .then(data => {
+        expect(data).to.eql(fixtureDeletedColumnsAndConcats);
+      });
+  });
+
+});
