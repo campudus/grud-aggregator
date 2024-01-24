3dee03290daff0dfb1f82836420face070dd2c9c
Max Stemplinger <ms@campudus.com>
2024-01-24 16:48:30 +0100

    fix: expose getEntitiesOfTables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export {createSchemaFromLanguageTables} from "./createSchemaFromLanguageTables";
 export {Database} from "./attachments/database";
 export {downloader} from "./attachments/downloader";
-export {getEntitiesOfTable} from "./entities";
+export {getEntitiesOfTable, getEntitiesOfTables} from "./entities";
 export {filter} from "./filter";
 export {findAttachments} from "./attachments/findAttachments";
 export {modifyImages} from "./attachments/modifyImage";
