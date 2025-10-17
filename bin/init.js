1c5ba1f472d9c12726c0abf76f1c7138f9a1bd60
smnhgn <simon.haugen@campudus.com>
2025-10-17 14:01:52 +0200

    feat: add init script for grud structure (wip)

---
+++ bin/init.js
@@ -0,0 +1,72 @@
+import fs from "node:fs/promises";
+import { parseArgs } from "node:util";
+import path from "node:path";
+import prettier from "prettier";
+import _ from "lodash";
+
+const options = {
+  url: { type: "string" },
+  dir: { type: "string" }
+};
+const { values } = parseArgs({ options });
+const { url, dir = "./" } = values;
+
+if (!url) {
+  throw new Error("argument 'url' is missing");
+}
+
+const structureResponse = await fetch(`${url}/structure`);
+const { tables } = await structureResponse.json();
+
+// cleanup structure
+const cleanColumn = (col, columnIndex) => {
+  if (_.isNumber(columnIndex)) {
+    col.index = columnIndex;
+  }
+
+  delete col.displayName;
+  delete col.ordering;
+  delete col.identifier;
+  delete col.description;
+  delete col.attributes;
+  delete col.constraint;
+  delete col.separator;
+  delete col.hidden;
+
+  if (col.toColumn) {
+    cleanColumn(col.toColumn);
+  }
+
+  col.concats?.forEach(cleanColumn);
+  col.groups?.forEach(cleanColumn);
+};
+
+for (const table of tables) {
+  delete table.displayName;
+  delete table.description;
+  delete table.attributes;
+  delete table.group;
+  delete table.hidden;
+
+  table.columns?.forEach(cleanColumn);
+}
+
+// format
+for (const table of tables) {
+  table.columns = _.keyBy(table.columns, "name");
+}
+
+const formatTS = (content) => prettier.format(content, { tabWidth: 2, parser: "typescript" });
+
+const tablesContent = await formatTS(
+  `const tables = ${JSON.stringify(_.keyBy(tables, "name"), null, 2)} as const;`
+);
+
+const dirPath = path.join(dir, ".grud-aggregator");
+const structurePathSource = path.join(import.meta.dirname, "structure.d.ts");
+const structurePathTarget = path.join(dirPath, "structure.d.ts");
+const structureTemplate = await fs.readFile(structurePathSource, "utf8");
+const structureContent = await formatTS(`\n${tablesContent}\n\n${structureTemplate}`);
+
+await fs.mkdir(dirPath, { recursive: true });
+await fs.writeFile(structurePathTarget, structureContent);
---
+++ bin/structure.d.ts
@@ -0,0 +1,114 @@
+declare module "grud-aggregator/structure" {
+  /* eslint-disable no-undef */
+  export const TABLES = tables;
+
+  // Utilities
+  type Prettify<T> = T extends object ? { [K in keyof T]: Prettify<T[K]> } : T;
+
+  type BuildArray<Length extends number, Acc extends unknown[] = []> = Acc["length"] extends Length
+    ? Acc
+    : BuildArray<Length, [...Acc, unknown]>;
+
+  type Increment<N extends number> = [...BuildArray<N>, unknown]["length"];
+
+  type Tuple<Obj, Index = 0, T = []> = Index extends keyof Obj
+    ? Tuple<Exclude<Obj, Index>, Increment<Index>, [...T, Obj[Index]]>
+    : T;
+
+  // Definitions
+  export type TableMap = typeof TABLES;
+  export type TableName = keyof TableMap;
+  export type Table<TName extends TableName = TableName> = TableMap[TName];
+  export type ColumnName<TName extends TableName> = keyof Table<TName>["columns"];
+
+  type AllColumns = {
+    [TName in TableName]: {
+      [CName in ColumnName<TName>]: Table<TName>["columns"][CName];
+    }[ColumnName<TName>];
+  }[TableName];
+
+  export type Column<
+    TName extends TableName = TableName,
+    CName extends ColumnName<TName> = ColumnName<TName>
+  > = TableName extends TName
+    ? AllColumns
+    : {
+        [T in TName]: {
+          [C in CName]: Table<T>["columns"][C];
+        }[CName];
+      }[TName];
+
+  type ColumnKind = Column["kind"];
+  type LanguageType = Extract<Column, { languageType: string }>["languageType"];
+  type LanguageTypeKey<LType extends LanguageType> = {
+    language: Langtag;
+    country: CountryCode;
+  }[LType];
+  type CountryCode = Extract<Column, { kind: "currency" }>["countryCodes"][number];
+  type Langtag = Table["langtags"][number];
+  type MultilangValue<Key extends Langtag | CountryCode, Value> = Prettify<
+    Partial<Record<Key, Value>>
+  >;
+
+  export type Attachment = Prettify<{
+    ordering: number;
+    url: MultilangValue<Langtag, string>;
+    uuid: string;
+    folder: number | null;
+    folders: number[];
+    title: MultilangValue<Langtag, string>;
+    description: MultilangValue<Langtag, string>;
+    internalName: MultilangValue<Langtag, string>;
+    externalName: MultilangValue<Langtag, string>;
+    mimeType: MultilangValue<Langtag, string>;
+    createdAt: string; // ISO
+    updatedAt: string; // ISO
+  }>;
+
+  type RowValue<
+    TName extends TableName = TableName,
+    CName extends ColumnName<TName> = ColumnName<TName>,
+    Col = Column<TName, CName>,
+    Kind = Col["kind"],
+    IsMultiLang = Col["multilanguage"],
+    TNameLink = Extract<Table, { id: Col["toTable"] }>["name"],
+    CNameLink = Col["toColumn"]["name"],
+    ConcatCols = Col["concats"][number]
+  > = {
+    boolean: IsMultiLang extends true ? MultilangValue<Langtag, boolean> : boolean | null;
+    shorttext: IsMultiLang extends true ? MultilangValue<Langtag, string> : string | null;
+    text: IsMultiLang extends true ? MultilangValue<Langtag, string> : string | null;
+    richText: IsMultiLang extends true ? MultilangValue<Langtag, string> : string | null;
+    numeric: IsMultiLang extends true ? MultilangValue<Langtag, number> : number | null;
+    currency: IsMultiLang extends true ? MultilangValue<CountryCode, number> : number | null;
+    date: string;
+    datetime: string;
+    attachment: Attachment[];
+    concat: Tuple<{
+      [ColIndex in ConcatCols["index"]]: RowValue<
+        TName,
+        Extract<ConcatCols, { index: ColIndex }>["name"]
+      >;
+    }>;
+    link: { id: number; value: RowValue<TNameLink, CNameLink> }[];
+    group: unknown[];
+  }[Kind];
+
+  export type Row<
+    TName extends TableName = TableName,
+    CName extends ColumnName<TName> = ColumnName<TName>,
+    Col = Column<TName, CName>,
+    ColIndizes = Col["index"]
+  > =
+    ColumnName<TName> extends CName
+      ? {
+          id: number;
+          values: Tuple<{
+            [ColIndex in ColIndizes]: RowValue<TName, Extract<Col, { index: ColIndex }>["name"]>;
+          }>;
+        }
+      : {
+          id: number;
+          values: [RowValue<TName, CName>];
+        };
+}
